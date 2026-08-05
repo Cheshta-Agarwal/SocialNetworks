0a9352bc160a54d088740c72870649f7080e1274
Cheshta-Agarwal <agarwalcheshta2050@gmail.com>
2026-08-05 06:50:59 +0530

    configured base url in vite.config.ts

--- socialgraph-lab/vite.config.ts
+++ socialgraph-lab/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', 
+  base: '/SocialNetworks/socialgraph-lab/', 
   plugins: [
     react(),
     tailwindcss(),
